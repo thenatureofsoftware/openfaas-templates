42897ad5c4a6fbf99e036f22e46855fb2b6934e0
Lars Mogren <lars.mogren@gmail.com>
2019-01-02 19:52:04 +0100

    Adds a sample controller.

---
+++ template/kotlin-maven-mn/function/src/main/kotlin/sample/HelloWorldController.kt
@@ -0,0 +1,20 @@
+package sample
+
+import io.micronaut.http.MediaType
+import io.micronaut.http.annotation.Body
+import io.micronaut.http.annotation.Consumes
+import io.micronaut.http.annotation.Controller
+import io.micronaut.http.annotation.Post
+
+@Controller("/")
+class HelloWorldController {
+
+    @Consumes(MediaType.TEXT_PLAIN)
+    @Post(produces = [MediaType.TEXT_PLAIN])
+    fun index(@Body() msg: String): String {
+        return """
+            Hello World from Kotlin Micronaut!
+
+        """.trimIndent()
+    }
+}
