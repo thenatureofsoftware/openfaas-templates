aca9632f44c93c077092db467eaf190481fce3df
Lars Mogren <lars.mogren@gmail.com>
2019-01-06 21:36:21 +0100

    Example function now reads from stdin.

--- template/kotlin-graalvm-native/function/src/main/kotlin/function/Fn.kt
+++ template/kotlin-graalvm-native/function/src/main/kotlin/function/Fn.kt
@@ -9,10 +9,15 @@ class Fn {
         return """
             Hello from Kotlin GraalVM Native!
             Message: $message
-        """.trimIndent()
+
+        """.trimIndent() + System.getenv().entries.map { "${it.key}=${it.value}" }.joinToString(separator = "\n")
     }
 }
 
 fun main(args: Array<String>) {
-    println(Fn().hello(args.joinToString(separator = " ")))
+    val lines = mutableListOf<String>()
+    while(System.`in`.available() > 0) {
+        lines.add(readLine() ?: "")
+    }
+    println(Fn().hello(lines.joinToString(separator = "\n")))
 }
